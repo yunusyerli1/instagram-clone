f26e8739c68f1b8040f01c12fc44e6726b7cb2e8
yunusyerli1 <yunus.yerli@gaboras.com>
2022-08-25 15:57:13 +0300

    after login started

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,28 @@
-
+import Icon from "components/Icon";
+import {AiFillCloseCircle} from "react-icons/ai"
+import {useState} from "react";
+import classNames from "classnames";
+import SVG from "react-inlinesvg";
 
 export default function Search() {
-    return(
-        <form className="w-[300px]">
-            <input type="text" placeholder="Search"/>
-        </form>
-    );
+
+	const [open, setOpen] = useState(false)
+
+	return (
+		<div className="w-[268px] relative">
+			<span className={classNames({
+				"absolute text-[#8e8e8e] pointer-events-none top-0 left-0 h-9 w-9 flex items-center justify-center": true,
+				"hidden": open
+			})}>
+				{/* <Icon name="search" /> */}
+			</span>
+			<input onFocus={() => setOpen(true)} onBlur={() => setOpen(false)} type="text" placeholder="Search" className="h-9 outline-none focus:pl-3 pl-9 w-full rounded bg-[#efefef]"/>
+			<button onClick={() => setOpen(false)} className={classNames({
+				"absolute text-[#c7c7c7] hidden top-0 right-0 w-9 h-9 items-center justify-center": true,
+				"!flex": open
+			})}>
+				<AiFillCloseCircle />
+			</button>
+		</div>
+	)
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -8,7 +8,7 @@ import {LoginSchema} from "validation";
 
 export default function Login() {
 
-  const navigate = useNavigate()
+	const navigate = useNavigate()
 	const location = useLocation()
 	const ref = useRef()
 
@@ -28,37 +28,42 @@ export default function Login() {
 		}
 	}, [ref])
 
-    const images = [
-      'https://www.instagram.com/static/images/homepage/screenshots/screenshot1-2x.png/cfd999368de3.png',
-      'https://www.instagram.com/static/images/homepage/screenshots/screenshot2-2x.png/80b8aebdea57.png',
-      'https://www.instagram.com/static/images/homepage/screenshots/screenshot3-2x.png/fe2540684ab2.png',
-      'https://www.instagram.com/static/images/homepage/screenshots/screenshot4-2x.png/8e9224a71939.png'
-    ]
+	const images = [
+		'https://www.instagram.com/static/images/homepage/screenshots/screenshot1-2x.png/cfd999368de3.png',
+		'https://www.instagram.com/static/images/homepage/screenshots/screenshot2-2x.png/80b8aebdea57.png',
+		'https://www.instagram.com/static/images/homepage/screenshots/screenshot3-2x.png/fe2540684ab2.png',
+		'https://www.instagram.com/static/images/homepage/screenshots/screenshot4-2x.png/8e9224a71939.png'
+	]
 
-    const handleSubmit = async (values, actions) => {
-      await login(values.username, values.password)
-      navigate(location.state?.return_url || '/', {
-        replace: true
-      })
-    }
-  
-    return (
-      <div className="h-full w-full flex flex-wrap overflow-auto items-center gap-x-8 justify-center">
-        <div className="hidden md:block w-[380px] h-[581px] bg-logo-pattern relative bg-[length:468.32px_634.15px] bg-[top_left_-46px]">
-          <div className="w-[250px] h-[538px] absolute top-[27px] right-[18px]" ref={ref}>
-            <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-700 ease-linear" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png" alt=""/>
-            <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-700 ease-linear" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot2.png/4d62acb667fb.png" alt=""/>
-            <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-700 ease-linear" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot3.png/94edb770accf.png" alt=""/>
-            <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-700 ease-linear" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot4.png/a4fd825e3d49.png" alt=""/>
-          </div>
-        </div>
-        <div className="w-[350px] grid gap-y-3">
-          <div className="bg-white border px-[40px] pt-10 pb-6">
-            <a href="/" className="flex justify-center mb-8">
-              <img className="h-[51px]" src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png" alt=""/>
-            </a>
+	const handleSubmit = async (values, actions) => {
+		await login(values.username, values.password)
+		navigate(location.state?.return_url || '/', {
+			replace: true
+		})
+	}
 
-            <Formik
+	return (
+		<div className="h-full w-full flex flex-wrap overflow-auto items-center gap-x-8 justify-center">
+
+			<div
+				className="hidden md:block w-[380px] h-[581px] bg-logo-pattern relative bg-[length:468.32px_634.15px] bg-[top_left_-46px]">
+				<div className="w-[250px] h-[538px] absolute top-[27px] right-[18px]" ref={ref}>
+					{images.map((image, key) => (
+						<img key={key} className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
+								 src={image}
+								 alt=""/>
+					))}
+				</div>
+			</div>
+
+			<div className="w-[350px] grid gap-y-3">
+
+				<div className="bg-white border px-[40px] pt-10 pb-6">
+					<a href="#" className="flex justify-center mb-8">
+						<img className="h-[51px]"
+								 src="https://www.instagram.com/static/images/web/logged_out_wordmark-2x.png/d2529dbef8ed.png" alt=""/>
+					</a>
+					<Formik
 						validationSchema={LoginSchema}
 						initialValues={{
 							username: '',
@@ -91,14 +96,14 @@ export default function Login() {
 							</Form>
 						)}
 					</Formik>
-          </div>
-          <div className="bg-white border p-4 text-sm text-center">
-          Don't have an account? <a href="/" className="font-semibold text-brand">Sign up</a> 
-        </div>
-        </div>
-        
-        
-        
-      </div>
-    );
+				</div>
+
+				<div className="bg-white border p-4 text-sm text-center">
+					Don't have an account? <a href="#" className="font-semibold text-brand">Sign up</a>
+				</div>
+
+			</div>
+
+		</div>
+	);
 }
